753f67a84fe84b0b23b182889cec70644b5d7d41
Tomas Tzorta UNI <greekharvarddev@gmail.com>
2024-02-09 22:03:47 +0000

    Player Controller
    - Got the Subsystem
    - Added Mapping Context
    - Changed Data such as mouse cursor visibility
    - And Set Input mode for UI and Game

---
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -0,0 +1,31 @@
+// Copyright Always NAT20
+
+
+#include "Player/AuraPlayerController.h"
+
+#include "EnhancedInputSubsystems.h"
+
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	check(AuraContext);
+
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+
+	Subsystem->AddMappingContext(AuraContext, 0);
+
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock); //don't lock mouse to viewport
+	InputModeData.SetHideCursorDuringCapture(false); //don't hide cursor
+	SetInputMode(InputModeData);
+}
