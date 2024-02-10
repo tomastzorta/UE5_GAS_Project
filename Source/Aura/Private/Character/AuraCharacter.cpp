81264c922bbd7bed252b599f8b36ab3c1a12dfc4
Tomas Tzorta UNI <greekharvarddev@gmail.com>
2024-02-10 02:07:05 +0000

    Game Mode
    - Made C++ Class
    - Derived in BP
    - Set Controller and Pawn
    - Setup Camera and Spring Arm
    - Fixed Idle Animation

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -3,3 +3,16 @@
 
 #include "Character/AuraCharacter.h"
 
+#include "GameFramework/CharacterMovementComponent.h"
+
+AAuraCharacter::AAuraCharacter()
+{
+	GetCharacterMovement()->bOrientRotationToMovement = true; // Character moves in the direction of input
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 400.0f, 0.0f); // At this rotation rate
+	GetCharacterMovement()->bConstrainToPlane = true; // Constrain movement to the XY plane (for top down games)
+	GetCharacterMovement()->bSnapToPlaneAtStart = true; // Snap to the XY plane at the start (for top down games)
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+}
---
+++ Source/Aura/Private/Game/AuraGameModeBase.cpp
@@ -0,0 +1,5 @@
+// Copyright Always NAT20
+
+
+#include "Game/AuraGameModeBase.h"
+
